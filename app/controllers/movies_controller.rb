cbdc00876d66d48c75d0818c96f44d310471aeb3
justin <davegle86@gmail.com>
2017-03-23 17:24:22 +0800

    implement new/edit/create function

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,30 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def show
+    @movie = Movie.find[params[:id]]
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+      redirect_to movies_path
+  end
+
+
+private
+
+def movie_params
+  params.require(:movie).permit(:title, :description)
+end
+
 end
