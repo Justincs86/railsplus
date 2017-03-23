f94e585d89d3d69fedf174e06c1c2448ddfb2bab
justin <davegle86@gmail.com>
2017-03-23 17:39:05 +0800

    implement movies#update & destroy

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,6 +21,18 @@ class MoviesController < ApplicationController
       redirect_to movies_path
   end
 
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+      redirect_to movies_path
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+      redirect_to movies_path
+  end
+
 
 private
 
